a43e9a2874a182db6b05df63695c6e76aacd11c0
riaria <riaiswandari51@gmail.com>
2024-02-20 17:56:02 +0800

    tes

---
+++ admin.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8>
+    <meta name="viewport" content="width=device-width,initial-scale=1.0"
+    <title>document</title>
+</head>
+<body>
+<h1>HALAMAN ADMIN<h1>
+<a href="">tambah data</a>
+<table border=1>
+ <thead>
+    <tr>
+     <th>fitur</th>
+     <th>user</th>
+   </tr>
+   <tr>
+      <td>login</td>
+      <td>x</td>
+      
+   </tr>
+   <tr>
+      <td>logout</td>
+      <td>x</td>
+   </tr> 
+   <tr>
+      <td>Registrasi</td>
+      <td>x</td>
+   </tr>
+   <tr>
+      <td>Data Foto/galery</td>
+      <td>x</td>
+   </tr>
+   <tr>
+      <td>Hapus Foto</td>
+      <td></td>
+   </tr>
+   <tr>
+      <td>hapus Foto</td>
+      <td>x</td>
+</tr>  
+</thead>
+<body>
+</body
